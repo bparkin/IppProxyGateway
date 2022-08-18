808a92bf3225691d5f53854f2942733d73e41110
Brian Parkin <brian.parkin@parchment.com>
2022-08-18 16:20:40 -0700

    Adding spring cloud gateway with application.yml routes

---
+++ src/main/java/com/parchment/ippproxygateway/IppProxyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.parchment.ippproxygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IppProxyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IppProxyGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parchment/ippproxygateway/IppProxyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parchment.ippproxygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IppProxyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
